0ff95081ec76090883c558dae55ac426ec30e90b
Rmadeye <rafmadaj@gmail.com>
2019-11-21 13:55:41 +0100

    Deploy-ready

--- exec.py
+++ exec.py
@@ -1,5 +1,5 @@
-from webapp import app as application
+from webapp.app import app as application
 
-if __name__ == "__main__":
-    application.app.run()
-
+# if __name__ == "__main__":
+#     application.app.run()
+#
